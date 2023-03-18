d776db8dc85f59c4243c186c7afb4079794d7132
Nam Bui <bvnam98@gmail.com>
2023-03-18 22:24:59 +0700

    update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,6 @@ import { Main } from '@/templates/Main';
 
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
-    
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
